0d88bcdfc05ec612d2b177ff1c30b3967590e832
KennyTM~ <kennytm@gmail.com>
2011-01-20 23:14:47 +0800

    ipsw_decrypt: Unzip and recognize the content of BuildManifest.plist

--- ipsw_decrypt.py
+++ ipsw_decrypt.py
@@ -18,6 +18,37 @@
 #
 
 from optparse import OptionParser
+from tempfile import mkdtemp
+from zipfile import ZipFile
+from contextlib import closing
+import shutil
+import os
+import os.path
+from plistlib import readPlist
+
+
+class TemporaryDirectory(object):
+    def __init__(self):
+        self._should_del = True
+        pass
+        
+    def __enter__(self):
+        self._tempdir = mkdtemp()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._should_del:
+            shutil.rmtree(self._tempdir)
+            
+    def move(self, target_dir):
+        os.rename(self._tempdir, target_dir)
+        self._should_del = False
+        self._tempdir = target_dir
+        
+    @property
+    def directory(self):
+        return self._tempdir
+
 
 def parse_options():
     parser = OptionParser(usage='usage: %prog [options] path/to/ipsw', version='%prog 0.0')
@@ -35,11 +66,73 @@ def parse_options():
     
     return (options, args)
 
+
+_products = {
+    'AppleTV2,1': 'Apple TV 2G',
+    'iPad1,1': 'iPad',
+    'iPhone1,1': 'iPhone',
+    'iPhone1,2': 'iPhone 3G',
+    'iPhone2,1': 'iPhone 3GS',
+    'iPhone3,1': 'iPhone 4',
+    'iPod1,1': 'iPod touch 1G',
+    'iPod2,1': 'iPod touch 2G',
+    'iPod3,1': 'iPod touch 3G',
+    'iPod4,1': 'iPod touch 4G',
+}
+
+
 def main():
     (options, args) = parse_options()
     
     ipsw_path = args[0]
-
+    
+    output_dir = options.output
+    should_extract = True
+    
+    if os.path.isdir(output_dir):
+        build_manifest_file = os.path.join(output_dir, 'BuildManifest.plist')
+        if os.path.exists(build_manifest_file):
+            print("<Notice> Output directory '{0}' already exists. Assuming the IPSW has been extracted to this directory.".format(output_dir))
+            should_extract = False
+        else:
+            print("<Warning> Output directory '{0}' already exists.".format(output_dir))
+        
+        
+    if should_extract:
+        with TemporaryDirectory() as td:
+            print("<Info> Extracting content from {0}, it may take a minute...".format(ipsw_path))
+            with closing(ZipFile(ipsw_path)) as zipfile:
+                zipfile.extractall(td.directory)
+    
+            if output_dir is None:
+                build_manifest_file = os.path.join(td.directory, 'BuildManifest.plist')
+                plist_obj = readPlist(build_manifest_file)
+                product_type = plist_obj['SupportedProductTypes'][0]
+                product_name = _products.get(product_type, product_type)
+                version = plist_obj['ProductVersion']
+                build = plist_obj['ProductBuildVersion']
+                
+                output_dir = '{0}, {1} ({2})'.format(product_name, version, build)
+            
+            td.move(output_dir)
+            
+    build_manifest_file = os.path.join(output_dir, 'BuildManifest.plist')
+    plist_obj = readPlist(build_manifest_file)
+    
+    product_type = plist_obj['SupportedProductTypes'][0]
+    product_name = _products.get(product_type, product_type)
+    version = plist_obj['ProductVersion']
+    
+    build_identity = plist_obj['BuildIdentities'][0]
+    build_info = build_identity['Info']
+    build_train = build_info['BuildTrain']
+    build_number = build_info['BuildNumber']
+    device_class = build_info['DeviceClass']
+    
+    print("<Info> {0} ({1}), class {2}".format(product_name, product_type, device_class))
+    print("<Info> iOS version {0}, build {1} {2}".format(version, build_train, build_number))
+    
+    
 
     
 if __name__ == '__main__':
